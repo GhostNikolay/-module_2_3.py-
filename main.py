7c8caae3d083584801d9b576449a9e4461a84a27
GhostNikolay <snl-10@mail.ru>
2024-09-10 20:55:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-while True:
-    my_list = ['42', '69', '322', '13', '0', '99', '-5', '9', '8', '7', '-6', '5']
-    number = int(input('Введите число'))
-    if number > 0:
-        print('Число положительное')
-    else:
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+start = 0
+positive = []
+while start < len(my_list):
+    if 0 == my_list[start]:
+        start += 1
+    positive.append(my_list[start])
+    start += 1
+    print(positive)
+    if my_list[start] < 0:
         break
+
+
+
+
